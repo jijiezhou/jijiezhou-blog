3887635464eba63881c7251dcc5dafe1b86d62d7
jijiezhou <jijiez@email.unc.edu>
2024-02-28 21:01:14 -0500

    Cracking Leetcode directory outside

--- .vuepress/theme/components/Footer.vue
+++ .vuepress/theme/components/Footer.vue
@@ -7,7 +7,7 @@
             </a>
         </div>
         <div class="copy-right">
-           <span class="name">{{`${currentYear} 编程导航 &nbsp; |  &nbsp; `}} </span>
+           <span class="name">{{`${currentYear} Blog &nbsp; |  &nbsp; `}} </span>
             <a :href="government.href" target="_blank" rel="noreferrer" >
               {{government.name}}
             </a>
